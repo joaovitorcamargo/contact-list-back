82f7fe68c8edf9e50388aab5ddc940d187e2d21c
joaovitorcamargo <joaocamargo06@gmail.com>
2024-01-05 00:40:41 -0300

    feature: create routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\PeopleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('people')->group(function () {
+    Route::get('/',  [PeopleController::class, 'getAllPeoples']);
+    Route::post('/',  [PeopleController::class, 'registerPeople']);
+    Route::patch('/{id}',  [PeopleController::class, 'editPeople']);
+    Route::delete('/{id}',  [PeopleController::class, 'deletePeople']);
+});
+
+Route::prefix('contact')->group(function () {
+    Route::delete('/{id}',  [ContactController::class, 'deleteContact']);
 });
